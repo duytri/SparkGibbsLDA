0cc01fa31a88d19aa4c817f587222edac483ff84
nguyenduy <nguyenduy@NGUYENDUY-UBUNTU>
2017-05-13 00:56:51 +0700

    Basically run in Spark

--- SparkGibbsLDA/build.sbt
+++ SparkGibbsLDA/build.sbt
@@ -8,7 +8,7 @@ organization := "uit.master.thesis"
 scalaVersion := "2.11.8"
 
 libraryDependencies ++= Seq(
-  "org.apache.spark" %% "spark-core_2.11" % "2.1.1"
+  "org.apache.spark" %% "spark-core" % "2.1.1"
 )
 
 unmanagedBase <<= baseDirectory { base => base / "libs" }
--- SparkGibbsLDA/src/main/scala/SparkGibbsLDA.scala
+++ SparkGibbsLDA/src/main/scala/SparkGibbsLDA.scala
@@ -1,7 +1,19 @@
 package main.scala
 
+import org.apache.spark.SparkConf
+import org.apache.spark.SparkContext
+
 object SparkGibbsLDA {
   def main(args: Array[String]): Unit = {
-    println("Hello world!")
+    println("Collapsed Gibbs sampling LDA in Apache Spark.")
+
+    val startTime = System.currentTimeMillis()
+
+    //~~~~~~~~~~~ Spark ~~~~~~~~~~~
+    val conf = new SparkConf().setAppName("SparkGibbsLDA").setMaster("local[4]")
+    val sc = new SparkContext(conf)
+    val data = Array(1, 2, 3, 4, 5)
+    val distData = sc.parallelize(data)
+    println("Tong cua mang la: " + distData.reduce(_ + _))
   }
 }
